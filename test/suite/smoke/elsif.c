fad57d3dfb68d89d7a19150578cf1013bd8b4e93
benedikt.huber <benedikt.huber@gmail.com>
2008-06-05 14:04:49 +0000

    simple smoke tests

---
+++ test/suite/smoke/elsif.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+  int inp = 3;
+  if (0 == inp) { } 
+  else if (1 == inp) { } 
+  else if (2 == inp) { } 
+  else if (3 == inp) { printf("inp=3\n"); } 
+  else if (4 == inp) { } 
+  else if (5 == inp) { } 
+  else if (6 == inp) { }
+}

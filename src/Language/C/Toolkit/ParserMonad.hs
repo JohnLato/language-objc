684d3dc48925b07f5cbb6b6e6cfa5e0197594f13
benedikt.huber <benedikt.huber@gmail.com>
2008-06-27 10:18:43 +0000

    ParserMonad, Idents: Rename UNames, -Wall clean

--- src/Language/C/Toolkit/ParserMonad.hs
+++ src/Language/C/Toolkit/ParserMonad.hs
@@ -38,7 +38,7 @@ module Language.C.Toolkit.ParserMonad (
 
 import Language.C.Toolkit.Position  (Position(..))
 import Language.C.Toolkit.Errors    (interr)
-import Language.C.Toolkit.UNames         (Name)
+import Language.C.Toolkit.Names     (Name)
 import Language.C.Toolkit.Idents    (Ident)
 
 import Data.Set  (Set)
@@ -71,7 +71,7 @@ execParser :: P a -> String -> Position -> [Ident] -> [Name]
 execParser (P parser) input pos builtins names =
   case parser initialState of
     POk _ result -> Left result
-    PFailed message pos -> Right (message, pos)
+    PFailed message errpos -> Right (message, errpos)
   where initialState = PState {
           curPos = pos,
           curInput = input,
@@ -105,31 +105,31 @@ getPos :: P Position
 getPos = P $ \s@PState{curPos=pos} -> POk s pos
 
 addTypedef :: Ident -> P ()
-addTypedef ident = (P $ \s@PState{tyidents=tyidents} ->
-                             POk s{tyidents = ident `Set.insert` tyidents} ())
+addTypedef ident = (P $ \s@PState{tyidents=tyids} ->
+                             POk s{tyidents = ident `Set.insert` tyids} ())
 
 shadowTypedef :: Ident -> P ()
-shadowTypedef ident = (P $ \s@PState{tyidents=tyidents} ->
+shadowTypedef ident = (P $ \s@PState{tyidents=tyids} ->
                              -- optimisation: mostly the ident will not be in
                              -- the tyident set so do a member lookup to avoid
                              --  churn induced by calling delete
-                             POk s{tyidents = if ident `Set.member` tyidents
-                                                then ident `Set.delete` tyidents
-                                                else tyidents } ())
+                             POk s{tyidents = if ident `Set.member` tyids
+                                                then ident `Set.delete` tyids
+                                                else tyids } ())
 
 isTypeIdent :: Ident -> P Bool
-isTypeIdent ident = P $ \s@PState{tyidents=tyidents} ->
-                             POk s $! Set.member ident tyidents
+isTypeIdent ident = P $ \s@PState{tyidents=tyids} ->
+                             POk s $! Set.member ident tyids
 
 enterScope :: P ()
-enterScope = P $ \s@PState{tyidents=tyidents,scopes=ss} ->
-                     POk s{scopes=tyidents:ss} ()
+enterScope = P $ \s@PState{tyidents=tyids,scopes=ss} ->
+                     POk s{scopes=tyids:ss} ()
 
 leaveScope :: P ()
 leaveScope = P $ \s@PState{scopes=ss} ->
                      case ss of
                        []             -> interr "leaveScope: already in global scope"
-                       (tyidents:ss') -> POk s{tyidents=tyidents, scopes=ss'} ()
+                       (tyids:ss') -> POk s{tyidents=tyids, scopes=ss'} ()
 
 getInput :: P String
 getInput = P $ \s@PState{curInput=i} -> POk s i

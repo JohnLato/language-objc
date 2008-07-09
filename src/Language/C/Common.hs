e8792df1c5eb5a4a6b4ea0383c00949b33363e05
benedikt.huber <benedikt.huber@gmail.com>
2008-07-09 12:24:48 +0000

    Toolkit -> Common

---
+++ src/Language/C/Common.hs
@@ -0,0 +1,23 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Language.C.Toolkit
+-- Copyright   :  (c) 2008 Benedikt Huber
+-- License     :  BSD-style
+-- Maintainer  :  benedikt.huber@gmail.com
+-- Portability :  portable
+--
+-- Common utilities for Language.C
+-----------------------------------------------------------------------------
+module Language.C.Common (
+     module Language.C.Common.Node,
+     module Language.C.Common.Errors,
+     module Language.C.Common.Idents,
+     module Language.C.Common.Names,
+     module Language.C.Common.Position,
+)
+where
+import Language.C.Common.Node
+import Language.C.Common.Errors
+import Language.C.Common.Idents
+import Language.C.Common.Names
+import Language.C.Common.Position

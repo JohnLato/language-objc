e3ce0de5b9890cb696cc15d8251286f7ac7f2834
benedikt.huber <benedikt.huber@gmail.com>
2008-07-09 12:26:33 +0000

    Language.C.Common.Position: cleanup

--- src/Language/C/Toolkit/Position.hs
+++ src/Language/C/Toolkit/Position.hs
@@ -1,102 +1,92 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 -----------------------------------------------------------------------------
 -- |
--- Module      :  Language.C.Toolkit.Position
+-- Module      :  Language.C.Common.Position
 -- Copyright   :  (c) [1995..2000] Manuel M. T. Chakravarty
 -- License     :  BSD-style
 -- Maintainer  :  benedikt.huber@gmail.com
 -- Portability :  portable
 --
--- This module provides some definitions used throughout all modules of a
---  compiler. 
---
---  * May not import anything apart from `Config'.
+-- Source code position
 -----------------------------------------------------------------------------
-module Language.C.Toolkit.Position (
+module Language.C.Common.Position (
   --
   -- source text positions
   --
-  Position(Position), Pos (..), 
-  posFile,posRow,posColumn,
+  Position(Position),
+  posFile,posRow,posColumn,isSourcePos,
   nopos, isNopos,
-  dontCarePos,  isDontCarePos,
   builtinPos, isBuiltinPos,
   internalPos, isInternalPos,
   incPos, tabPos, retPos,
+  Pos(..),
 ) where
 import Data.Generics
 
--- uniform representation of source file positions; the order of the arguments
+-- | uniform representation of source file positions; the order of the arguments
 -- is important as it leads to the desired ordering of source positions
--- (EXPORTED) 
---
 data Position = Position String         -- file name
         {-# UNPACK #-}   !Int           -- row
         {-# UNPACK #-}   !Int           -- column
-  deriving (Eq, Ord)
-
+  deriving (Eq, Ord, Typeable, Data)
+    
 instance Show Position where
-  show (Position fname row col) = show (fname, row, col)
+  show pos@(Position fname row col) 
+    | isNopos pos = "<no file>"
+    | isBuiltinPos pos = "<builtin>"
+    | isInternalPos pos = "<internal>"
+    | otherwise = show (fname, row, col)
+
+-- | get the source file of the specified position. Fails unless @isSourcePos pos@.
 posFile :: Position -> String
 posFile (Position fname _ _) = fname
+-- | get the line number of the specified position. Fails if @isSourcePos pos@
 posRow  :: Position -> Int
 posRow (Position _ row _) = row
+-- | get the column of the specified position. Fails if @isSourcePos pos@
 posColumn :: Position -> Int
 posColumn (Position _ _ col) = col
 
--- no position (for unknown position information) (EXPORTED)
---
+class Pos a where
+    posOf :: a -> Position
+      
+isSourcePos :: Position -> Bool
+isSourcePos (Position _ row col) = row >= 0 && col >= 0      
+
+-- | no position (for unknown position information) 
 nopos :: Position
-nopos  = Position "<no file>" (-1) (-1)
+nopos  = Position "<no file>" (-1) 0
 
 isNopos :: Position -> Bool
-isNopos (Position _ (-1) (-1)) = True
-isNopos _                      = False
+isNopos (Position _ (-1) 0) = True
+isNopos _                   = False
 
--- don't care position (to be used for invalid position information) (EXPORTED)
---
-dontCarePos :: Position
-dontCarePos = Position "<invalid>" (-2) (-2)
-
-isDontCarePos  :: Position -> Bool
-isDontCarePos (Position _ (-2) (-2)) = True
-isDontCarePos _                      = False
-
--- position attached to objects that are hard-coded into the toolkit (EXPORTED)
+-- | position attached to built-in objects
 --
 builtinPos :: Position
-builtinPos  = Position "<built into the compiler>" (-3) (-3)
+builtinPos  = Position "<built into the parser>" (-1) 1
 
 isBuiltinPos :: Position -> Bool
-isBuiltinPos (Position _ (-3) (-3)) = True
-isBuiltinPos _                      = False
+isBuiltinPos (Position _ (-1) 1) = True
+isBuiltinPos _                   = False
 
--- position used for internal errors (EXPORTED)
---
+-- | position used for internal errors
 internalPos :: Position
-internalPos = Position "<internal error>" (-4) (-4)
+internalPos = Position "<internal error>" (-1) 2
 
 isInternalPos :: Position -> Bool
-isInternalPos (Position _ (-4) (-4)) = True
+isInternalPos (Position _ (-1) 2) = True
 isInternalPos _                      = False
 
--- instances of the class `Pos' are associated with some source text position
--- don't care position (to be used for invalid position information) (EXPORTED)
---
-class Pos a where
-  posOf :: a -> Position
-
--- advance column
---
+-- | advance column
 incPos :: Position -> Int -> Position
 incPos (Position fname row col) n = Position fname row (col + n)
 
--- advance column to next tab positions (tabs are at every 8th column)
---
+-- | advance column to next tab positions (tabs are considered to be at every 8th column)
 tabPos :: Position -> Position
 tabPos (Position fname row col) =
         Position fname row (col + 8 - (col - 1) `mod` 8)
 
--- advance to next line
---
+-- | advance to next line
 retPos :: Position -> Position
 retPos (Position fname row _col) = Position fname (row + 1) 1

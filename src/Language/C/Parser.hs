ee54f73d0ff64d15befde17f47d9a0c355d99137
benedikt.huber <benedikt.huber@gmail.com>
2008-07-21 21:54:50 +0000

    Refactoring: Common+AST -> Syntax (public API)

--- src/Language/C/Parser.hs
+++ src/Language/C/Parser.hs
@@ -7,18 +7,10 @@
 -- Maintainer  :  benedikt.huber@gmail.com
 -- Portability :  portable
 --
--- Language.C parser and pretty printer 
+-- Language.C parser
 -----------------------------------------------------------------------------
 module Language.C.Parser (
-    module Language.C.Parser.AST,
-    module Language.C.Parser.Builtin,
-    module Language.C.Parser.InputStream,
-    module Language.C.Parser.Parser,
-    module Language.C.Parser.Pretty
+    parseC,
 )
 where
-import Language.C.Parser.AST
-import Language.C.Parser.Builtin
-import Language.C.Parser.InputStream
 import Language.C.Parser.Parser
-import Language.C.Parser.Pretty

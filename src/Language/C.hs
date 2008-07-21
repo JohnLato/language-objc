3de542a275783d113fa2d7b51814b23b9c685303
benedikt.huber <benedikt.huber@gmail.com>
2008-07-21 21:57:22 +0000

    Language.C facade: stripped public API

--- src/Language/C.hs
+++ src/Language/C.hs
@@ -8,21 +8,16 @@
 --                   Betram Felgenhauer
 -- License     :  BSD-style
 -- Portability :  portable
+-- Stability   :  provisional
 --
 -- The C99 parser and pretty printer library.
 -----------------------------------------------------------------------------
 module Language.C (
-module Language.C.Common.Ident,
-module Language.C.Common.Position,
-module Language.C.Parser.InputStream,
-module Language.C.Parser.Parser,
-module Language.C.Parser.AST,
-module Language.C.Parser.Pretty,
+    module Language.C.Syntax,
+    module Language.C.Pretty,
+    module Language.C.Parser    
 )
 where
-import Language.C.Common.Ident
-import Language.C.Common.Position
-import Language.C.Parser.InputStream
-import Language.C.Parser.Parser
-import Language.C.Parser.AST
-import Language.C.Parser.Pretty
+import Language.C.Syntax
+import Language.C.Pretty
+import Language.C.Parser    

044e4fcb4b5e0907da9d8a4e4b990d05abd92332
benedikt.huber <benedikt.huber@gmail.com>
2008-07-22 00:43:28 +0000

    Propagate renamings to NodeDerive.hs and derive.sh

--- src/NodeDerive.hs
+++ src/NodeDerive.hs
@@ -2,11 +2,11 @@
 
 module NodeDerive (makeCNode) where
 import Language.Haskell.TH.All
-import Language.C.Common.Node
-import Language.C.Common.Position
+import Language.C.Syntax.Node
+import Language.C.Syntax.Position
 makeCNode :: Derivation
 makeCNode = derivation genNodeInst "CNode"
-nodeInfoTypeName = "Language.C.Common.Node.NodeInfo"
+nodeInfoTypeName = "Language.C.Syntax.Node.NodeInfo"
 genNodeInst :: DataDef -> [Dec]
 genNodeInst dat = [
     instance_context ["CNode"] "CNode" dat 
@@ -20,7 +20,7 @@ posOfDef = [Clause [VarP (mkName "x")]
                    []]
     
 -- If we have a data constructor
--- X a_1 .. a_n, and execatly one a_k is a Language.C.Common.NodeInfo, then return that a_k
+-- X a_1 .. a_n, and execatly one a_k is a Language.C.Syntax.NodeInfo, then return that a_k
 -- If we have a data constructor
 -- X a, then return nodeInfo a
 -- otherwise fail Dec

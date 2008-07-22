c36f45d51ba26b5c2f8e3c20307fcc0053ef8b50
benedikt.huber <benedikt.huber@gmail.com>
2008-07-22 00:48:58 +0000

    Language.C.Analysis.hs: SymbolTable -> DefTable

--- src/Language/C/Analysis.hs
+++ src/Language/C/Analysis.hs
@@ -5,18 +5,15 @@
 -- Copyright   :  (c) 2008 Benedikt Huber
 -- License     :  BSD-style
 -- Maintainer  :  benedikt.huber@gmail.com
--- Portability :  portable
+-- Stability   :  alpha, unstable  
 --
--- Semantics-oriented C source representation
+-- Analysis of the AST.
 -----------------------------------------------------------------------------
 module Language.C.Analysis (
-    module Language.C.Analysis.Analysis,
-    module Language.C.Analysis.SymbolTable,
     module Language.C.Analysis.SemRep,    
     module Language.C.Analysis.TravMonad,
 )
 where
-import Language.C.Analysis.Analysis
-import Language.C.Analysis.SymbolTable
-import Language.C.Analysis.SemRep   
+import Language.C.Analysis.DefTable
+import Language.C.Analysis.SemRep    
 import Language.C.Analysis.TravMonad

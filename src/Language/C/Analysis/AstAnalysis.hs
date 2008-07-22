ca7f828e213abecb2a24f2e27a264cbc75452e3a
benedikt.huber <benedikt.huber@gmail.com>
2008-07-22 14:36:16 +0000

    Analyzing declarations (initial import)

---
+++ src/Language/C/Analysis/AstAnalysis.hs
@@ -0,0 +1,234 @@
+{-# LANGUAGE ScopedTypeVariables, PatternGuards #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Language.C.Parser.Translation
+-- Copyright   :  (c) 2008 Benedikt Huber
+-- License     :  BSD-style
+-- Maintainer  :  benedikt.huber@gmail.com
+-- Portability :  portable (ScopedTypeVariables,PatternGuards)
+-- Stability   :  alpha
+--
+-- Analyse the parse tree
+--
+-- Traverses the AST, analyses declarations and invokes handlers.
+-----------------------------------------------------------------------------
+module Language.C.Analysis.AstAnalysis (
+    analyseAST,
+    analyseExt,analyseFunDef,analyseExtDecls,
+)
+where
+import Language.C.Syntax
+import Language.C.Syntax.RList
+import Language.C.Syntax.AST
+import Language.C.Syntax.Constants
+import Language.C.Syntax.Ops
+import Language.C.Analysis.SemRep
+import Language.C.Analysis.TravMonad
+import Language.C.Analysis.DefTable
+import Language.C.Analysis.DeclAnalysis
+import Control.Monad
+import Control.Monad.State
+import Prelude hiding (reverse)
+import Data.Foldable (foldrM)
+import Data.List hiding (reverse)
+import Data.Maybe
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Debug.Trace
+
+
+-- * analysis
+
+-- | Analyse the given AST
+--
+-- @analyseAST ast@ results in global declaration dictionaries.
+-- If you want to perform specific actions on declarations or definitions, you may provide
+-- callbacks in the @MonadTrav@ @m@.
+--
+-- Returns the set of global declarations and definitions which where successfully translated.
+-- It is the users responsibility to check whether any hard errors occured (@runTrav@ does this for you).
+analyseAST :: (MonadTrav m) => CTranslUnit -> m GlobalDecls
+analyseAST (CTranslUnit decls node) = do
+    mapRecoverM_ analyseExt decls
+    liftM globalDefs getDefTable
+    where
+    mapRecoverM_ f = mapM_ (handleTravError . f)
+
+-- | Analyse an top-level declaration
+analyseExt :: (MonadTrav m) => CExtDecl -> m ()
+analyseExt (CAsmExt asm)     
+    = handleAsmBlock (convertStringLit asm)
+analyseExt (CFDefExt fundef) 
+    = analyseFunDef fundef 
+analyseExt (CDeclExt decls) 
+    = analyseExtDecls decls
+
+-- | Analyse a function definition
+-- data VarDeclInfo = VarDeclInfo VarName Bool {- is-inline? -} StorageSpec Attributes Type NodeInfo
+
+analyseFunDef :: (MonadTrav m) => CFunDef -> m ()
+analyseFunDef (CFunDef declspecs declr oldstyle_decls stmt node_info) = do
+    -- analyse the declarator
+    var_decl_info <- analyseVarDecl True declspecs declr oldstyle_decls
+    let (VarDeclInfo name is_inline storage_spec attrs ty declr_node) = var_decl_info
+    let ident = identOfVarName name
+    storage <- computeFunDefStorage ident storage_spec
+    let var_decl = VarDecl name (DeclAttrs is_inline storage attrs) ty
+    handleVarDecl (Decl var_decl node_info)
+    -- translate the body
+    stmt' <- analyseFunctionBody var_decl stmt
+    -- define the function
+    handleFunDef ident (FunDef var_decl stmt' node_info)
+    
+-- | Analyse a top-level declaration other than a function definition
+analyseExtDecls :: (MonadTrav m) => CDecl -> m ()
+analyseExtDecls (CDecl declspecs declrs node) 
+    | (Just declspecs') <- isTypeDef declspecs = 
+        case declrs of
+            [(Just tydeclr,Nothing,Nothing)] -> analyseTypeDef declspecs' tydeclr node
+            _ -> astError node "bad typdef declaration: declarator missing or bitfieldsize/initializer present"
+    | otherwise = do
+        -- analyse the declarators
+        mapM_ (uncurry (convertVarDeclr declspecs)) $ zip (True : repeat False) declrs
+    where
+    convertVarDeclr declspecs handle_sue_def (Just declr, init_opt, Nothing) = do
+        -- analyse the declarator
+        vardeclInfo@(VarDeclInfo _ _ _ _ typ _) <- analyseVarDecl handle_sue_def declspecs declr []
+        -- declare / define the object
+        init_opt' <- mapMaybeM init_opt tInit
+        when (isTypeOfExpr typ) $ astError node "we cannot analyse typeof(expr) yet"
+        if (isFunctionType typ) 
+            then extFunProto vardeclInfo
+            else extVarDecl vardeclInfo init_opt'
+    convertVarDeclr _ _ (Nothing,_,_)         = astError node "abstract declarator in object declaration"
+    convertVarDeclr _ _ (_,_,Just bitfieldSz) = astError node "bitfield size in object declaration"
+    isTypeOfExpr (TypeOfExpr _) = True
+    isTypeOfExpr _ = False
+    
+-- convert typedef
+analyseTypeDef :: (MonadTrav m) => [CDeclSpec] -> CDeclr -> NodeInfo -> m ()
+analyseTypeDef declspecs declr node_info = do
+    -- analyse the declarator
+    (VarDeclInfo name is_inline storage_spec attrs ty declr_node) <- analyseVarDecl True declspecs declr []
+    -- /TODO/: move attribute to the type
+    checkValidTypeDef is_inline storage_spec attrs
+    let ident = identOfVarName name
+    handleTypeDef (TypeDef' ident ty attrs node_info)
+    where
+    checkValidTypeDef True _ _ = astError node_info "inline specifier for typedef"
+    checkValidTypeDef _ _ (_:_) = astError node_info "attributes for typedefs aren't supported"
+    checkValidTypeDef _ NoStorageSpec _ = return ()
+    checkValidTypeDef _ bad_storage _ = astError node_info $ "storage specified for typedef: " ++ show bad_storage
+    
+-- | analyse declarators
+analyseVarDecl :: (MonadTrav m) => Bool -> [CDeclSpec] -> CDeclr -> [CDecl] -> m VarDeclInfo
+analyseVarDecl handle_sue_def declspecs declr oldstyle_params = do
+    let (storagespecs, typequals, typespecs, inline) = partitionDeclSpecs declspecs
+    -- analyse the storage specifiers
+    storage_spec  <- canonicalStorageSpec storagespecs
+    -- translate the type into semantic representation
+    typ          <- tType handle_sue_def node typequals typespecs derived_declrs oldstyle_params
+    -- translate attributes
+    attrs'       <- mapM tAttr attributes
+    -- create the variable name
+    name         <- mkVarName node nameOpt (fmap convertStringLit asmname_opt)
+    return $ VarDeclInfo name inline storage_spec attrs' typ node
+    where
+    (CDeclr nameOpt derived_declrs asmname_opt attributes node) = declr
+    isInlineSpec (CInlineQual _) = True
+    isInlineSpec _ = False
+
+tTypeDef declspecs tydeclr node = undefined -- TODO
+-- | compute storage of a function definition
+--
+-- a function definition has static storage with internal linkage if specified `static`,
+-- the previously declared linkage if any if 'extern' and 'extern' otherwise
+--
+-- This function won't raise an error, even if the function is already declared, because
+-- the error handling should be done in 'handleFunDef'
+computeFunDefStorage :: (MonadTrav m) => Ident -> StorageSpec -> m Storage
+computeFunDefStorage _ NoStorageSpec = return$ Static ExternalLinkage False
+computeFunDefStorage _ ThreadSpec    = return$ Static ExternalLinkage True
+computeFunDefStorage _ (StaticSpec b)  = return$ Static InternalLinkage b
+computeFunDefStorage ident (ExternSpec b)  = do
+     obj_opt <- lookupFun ident
+     return$ case obj_opt of
+                Just vardecl -> storage . declAttrs $ vardecl
+                _ -> Static ExternalLinkage b
+computeFunDefStorage _ bad_spec = error $ "unexpected storage specifier: "++show bad_spec
+
+-- | handle a function prototype
+extFunProto :: (MonadTrav m) => VarDeclInfo -> m ()
+extFunProto (VarDeclInfo var_name is_inline storage_spec attrs typ node_info) = 
+    do  old_fun <- lookupFun (identOfVarName var_name)
+        checkValidFunDecl
+        let decl = VarDecl var_name (DeclAttrs is_inline (funDeclLinkage old_fun) attrs) typ
+        handleVarDecl (Decl decl node_info)
+    where
+    funDeclLinkage old_fun =
+        case storage_spec of
+            NoStorageSpec    -> FunLinkage ExternalLinkage -- prototype declaration / external linkage
+            StaticSpec False -> FunLinkage InternalLinkage -- prototype declaration / internal linkage
+            ExternSpec False -> case old_fun of
+                                    Nothing -> FunLinkage ExternalLinkage
+                                    Just f  -> (storage.declAttrs) f
+            _ -> error $ "funDeclLinkage: " ++ show storage_spec
+    checkValidFunDecl
+        | isThreadLocalSpec storage_spec = astError node_info "thread local storage specified for function"
+        | RegSpec <- storage_spec        = astError node_info "invalid `register' storage specified for function"
+        | (not $ isFunctionType typ)     = error "function prototype without function type"      
+        | otherwise                      = return ()
+        
+-- | handle a object declaration \/ definition
+--
+-- We have to check the storage specifiers here, as they determine wheter we're dealing with decalartions
+-- or definitions
+-- see [doc/ExternalDefinitions.txt]
+extVarDecl :: (MonadTrav m) => VarDeclInfo -> (Maybe Initializer) -> m ()
+extVarDecl (VarDeclInfo var_name is_inline storage_spec attrs typ node_info) init_opt =
+    do let ident = identOfVarName var_name
+       old_decl <- lookupObject ident    
+       checkValidVarDeclStorage
+       let vardecl linkage = VarDecl var_name (DeclAttrs is_inline linkage attrs) typ
+       let decl linkage = Decl (vardecl linkage) node_info
+       case storage_spec of
+           NoStorageSpec           -- tentative if there is no initializer, external               
+               -> handleObjectDef ident $ ObjDef (vardecl (Static ExternalLinkage False)) init_opt node_info 
+           StaticSpec thread_local -- tentative if there is no initializer, internal 
+               -> handleObjectDef ident $ ObjDef (vardecl (Static InternalLinkage thread_local)) init_opt node_info
+           ExternSpec thread_local 
+             | Nothing <- init_opt  -- declaration with either external or old storage
+               -> handleVarDecl $ decl $ maybe (Static ExternalLinkage thread_local) (storage.declAttrs) old_decl
+             | otherwise            -- warning, external definition
+               -> do warn node_info "Both initializer and `extern` specifier given - treating as definition"
+                     handleObjectDef ident $ ObjDef (vardecl (Static ExternalLinkage thread_local)) init_opt node_info
+           _ -> error$ "extVarDecl: storage_spec: "++show storage_spec
+    where
+    checkValidVarDeclStorage
+        | is_inline               = astError node_info "invalide `inline' specifier for non-function"
+        | RegSpec <- storage_spec = astError node_info "invalid `register' storage specified for external object"
+        | otherwise               = return ()
+
+-- | /TODO/: Bogus
+analyseFunctionBody :: (MonadTrav m) => VarDecl -> CStat -> m Stmt
+analyseFunctionBody _ = return
+-- | /TODO/: Bogus
+tStmt :: (MonadTrav m) => CStat -> m Stmt
+tStmt = return
+-- | /TODO/: Bogus
+tExpr :: (MonadTrav m) => CExpr -> m Expr
+tExpr = return
+-- | /TODO/: Bogus
+tInit :: (MonadTrav m) => CInit -> m Initializer
+tInit = return
+
+
+-- return @Just declspecs\\typedef@ if the declaration is a typedef
+isTypeDef :: [CDeclSpec] -> Maybe [CDeclSpec]
+isTypeDef declspecs = 
+    case foldr isTypeDefSpec (False,[]) declspecs of
+        (True,specs') -> Just specs'
+        (False,_)     -> Nothing
+    where
+    isTypeDefSpec (CStorageSpec (CTypedef n)) (_,specs) = (True, specs)
+    isTypeDefSpec spec (b,specs) = (b,spec:specs)
--- src/Language/C/Analysis/Pretty.hs
+++ src/Language/C/Analysis/Pretty.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
+{-# OPTIONS -fno-warn-orphans #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Language.C.Analysis.Pretty
@@ -28,13 +29,11 @@ import qualified Data.Map as Map
 instance Pretty GlobalDecls where
     pretty gd = text "Global Declarations" $$ (nest 4 $ vcat declMaps)
         where
-        declMaps = [ prettyDecls "declarations" (Map.assocs $ gDecls gd),
+        declMaps = [ prettyMap "declarations" $ gDecls gd,
                      prettyMap "objects" $ gObjs gd,  prettyMap "functions" $ gFuns gd, 
                      prettyMap "tags"    $ gTags gd,  prettyMap "typedefs"  $ gTypedefs gd ]
         prettyMap :: (Pretty t, Pretty k) => String -> Map k t -> Doc
         prettyMap label = prettyAssocsWith label pretty pretty . Map.assocs
-        prettyDecls label = prettyAssocsWith label pretty $ \(decl,isFunction) ->
-                                text (if isFunction then "function: " else "object: ") <+> pretty decl
 prettyAssocsWith :: String -> (k -> Doc) -> (v -> Doc) -> [(k,v)] -> Doc
 prettyAssocsWith label prettyKey prettyVal theMap =
     text label $$ (nest 8) (vcat $ map prettyEntry theMap)
@@ -43,16 +42,14 @@ prettyAssocsWith label prettyKey prettyVal theMap =
 
 globalDeclStats :: (FilePath -> Bool) -> GlobalDecls -> [(String,Int)]
 globalDeclStats file_filter gmap =
-    [ ("Object Declarations",Map.size objDecls),
-      ("Object Definitions", Map.size objDefs),
-      ("Function Declarations", Map.size funDecls),
+    [ ("Object/Function Declarations",Map.size decls),
       ("Function Definitions", Map.size funDefs),
       ("Tag definitions", Map.size tagDefs),
       ("Typedefs", Map.size typeDefs)
     ] 
     where
     gmap' = filterGlobalDecls filterFile gmap
-    (funDecls, objDecls) = Map.partition (snd) $ (gDecls gmap')
+    decls = gDecls gmap'
     objDefs = gObjs gmap'
     funDefs = gFuns gmap'
     tagDefs = gTags gmap'
@@ -66,7 +63,7 @@ instance Pretty (Either VarDecl FunDef) where
 instance Pretty Ident where
     pretty = text . identToString
 instance Pretty SUERef where
-    pretty (AnonymousType name)   = text $ "$" ++ show name
+    pretty (AnonymousType name)   = text $ "$sue_" ++ show (nameId name)
     pretty (NamedType ident)      = pretty ident
 instance Pretty TagDef where
     pretty (CompTag compty) = pretty compty
@@ -79,18 +76,19 @@ instance Pretty IdentDecl where
     pretty (TypeDef tydef) = pretty tydef
 instance Pretty Decl where
     pretty (Decl vardecl _) = 
-        text "Declaration: " <+>
+        text "DECL" <+>
         pretty vardecl
 instance Pretty TypeDef where
-    pretty (TypeDef' ident ty _) = text "typedef" <+> pretty ident <+> text "as"  <+> pretty ty
+    pretty (TypeDef' ident ty attrs _) = 
+        text "typedef" <+> pretty ident <+> text "as"  <+> 
+        pretty attrs <+> pretty ty
 instance Pretty ObjDef where
     pretty (ObjDef vardecl init_opt _) = 
-        text "Object definition" <+> 
-        (maybe (text "(tentative)") (const empty) init_opt) <+> text ": " <+>
+        text "OBJ_DEF" <+> 
         pretty vardecl <+> maybe empty (((text "=") <+>) . pretty) init_opt
 instance Pretty FunDef where
     pretty (FunDef vardecl _stmt _) = 
-        text "Function definition: " <+>
+        text "FUN_DEF" <+>
         pretty vardecl
 instance Pretty VarDecl where
     pretty (VarDecl name attrs ty) = 
@@ -104,11 +102,15 @@ instance Pretty DeclAttrs where
         (hsep $ [ pretty storage, pretty attrs])
 
 instance Pretty Type  where  
-    pretty ty = prettyType "<obj>" ty
+    pretty ty = prettyType "<?>" ty
 prettyType declr_name ty = prettyTy (text declr_name) ty
     where
     prettyTy declr_name (DirectType ty_name quals attrs) = 
         pretty quals <+> pretty attrs <+> pretty ty_name <+> declr_name
+    prettyTy declr_name (TypeDefType (TypeDefRef ident _ _)) = 
+        text "typeref" <> parens (pretty ident) <+> declr_name
+    prettyTy declr_name (TypeOfExpr expr) = 
+        text "typeof" <> parens (pretty expr) <+> declr_name
     prettyTy declr_name (PtrType ty quals attrs) = 
         prettyTy empty ty <+>  text "*" <+> pretty attrs <+> pretty quals <+> declr_name
     prettyTy declr_name (ArrayType ty sz quals attrs) = 
@@ -118,6 +120,7 @@ prettyType declr_name ty = prettyTy (text declr_name) ty
         declr_name <+>
         parens (hsep . punctuate comma $ (map pretty params ++ (if variadic then [text ",..."] else []))) <+>
         pretty attrs
+
 instance Pretty TypeQuals where
     pretty tyQuals = hsep $ map showAttr [ ("const",constant),("volatile",volatile),("restrict",restrict) ]
         where showAttr (str,select) | select tyQuals = text str
@@ -130,21 +133,28 @@ instance Pretty TypeName where
     pretty TyVoid = text "void"
     pretty (TyIntegral int_type) = text (show int_type)
     pretty (TyFloating tyquals float_type) = pretty tyquals <+> text (show float_type)
-    pretty (TyComp (CompTypeDecl sue_ref tag  attrs _)) | null attrs = text (show tag) <+> text (show sue_ref)        
-                                                   | otherwise  = error "attributes for su decl"
-    pretty (TyEnum (EnumTypeDecl sue_ref attrs _)) | null attrs = text "enum" <+> text (show sue_ref)
-                                                   | otherwise  = error "attributes for enum decl"
-    pretty (TyTypeDef (TypeDefRef ident _)) = text "typeref" <> parens (pretty ident)
-    pretty (TyOfExpr expr) = text "typeof" <> parens (pretty expr)
+    pretty (TyComp (CompTypeDecl sue_ref tag _)) = text (show tag) <+> pretty sue_ref
+    pretty (TyEnum (EnumTypeDecl sue_ref _))     = text "enum" <+> pretty sue_ref
     pretty (TyBuiltin TyVaList) = text "va_list"
 instance Pretty FloatTypeQuals where
     pretty (NoFloatTypeQual) = empty
     pretty TyComplex = text "__complex__"
 
 instance Pretty CompType where
-    pretty _ = text "comptype"
+    pretty (CompType sue_ref tag members attrs node) =
+        (text.show) tag <+> pretty sue_ref <+> 
+        braces (terminateSemi members) <+>
+        pretty attrs
+instance Pretty MemberDecl where
+    pretty (MemberDecl (VarDecl name declattrs ty) bitfield _) = 
+        pretty declattrs <+> prettyType (show $ pretty name) ty <+>
+        (maybe empty (\bf -> text ":" <+> pretty bf) bitfield)
+    pretty (AnonBitField ty bitfield_sz _) =
+        pretty ty <+> text ":" <+> pretty bitfield_sz
+
 instance Pretty EnumType where
-    pretty _ = text "enumtype"
+    pretty (EnumType sue_ref enumerators attrs _) = 
+        text "enum" <+> pretty sue_ref <+> braces (terminateSemi enumerators) <+> pretty attrs
 instance Pretty Enumerator where
     pretty (ident,expr) = text "enumerator" <+> pretty ident
 instance Pretty Storage where
@@ -154,7 +164,7 @@ instance Pretty Storage where
     pretty (Static linkage thread_local) = 
         (hcat . punctuate (text "/") $ [ text "static",pretty linkage ])
         <+> (if thread_local then text ", __thread" else empty)
-    pretty (FunLinkage linkage) = hsep $ [ text "function", pretty linkage ]
+    pretty (FunLinkage linkage) = text "function/" <> pretty linkage
 instance Pretty Linkage where
     pretty InternalLinkage = text "internal"
     pretty ExternalLinkage = text "external"    
@@ -163,8 +173,13 @@ instance Pretty VarName where
     pretty (VarName ident asmname_opt) = pretty ident <+> (maybe empty pAsmName asmname_opt)
         where pAsmName asmname = text "" <+> parens (text "asmname" <+> pretty asmname)
 instance Pretty Attributes where
-    pretty = hsep . punctuate comma . map pretty
+    pretty = joinComma
 instance Pretty Attr where
-    pretty _ = text "<attribute>"
+    pretty (Attr ident es _) = pretty ident <+> (if null es then empty else text "(...)")
 instance Pretty AsmName where
     pretty = text.show.cstringOfLit
+    
+joinComma :: (Pretty a) => [a] -> Doc
+joinComma = hsep . punctuate comma . map pretty
+terminateSemi :: (Pretty a) => [a] -> Doc
+terminateSemi = hsep . map (<> semi) . map pretty 

5e8dc0e54e35f76281e93206f18c02292c141f56
benedikt.huber <benedikt.huber@gmail.com>
2008-07-22 11:27:39 +0000

    C.System: Create a class for preprocessors, improved option handling, simple parseFile wrapper

--- src/Language/C.hs
+++ src/Language/C.hs
@@ -13,6 +13,7 @@
 -- The C99 parser and pretty printer library.
 -----------------------------------------------------------------------------
 module Language.C (
+    parseFile,
     module Language.C.Syntax,
     module Language.C.Pretty,
     module Language.C.Parser,
@@ -23,3 +24,15 @@ import Language.C.Syntax
 import Language.C.Pretty
 import Language.C.Parser    
 import Language.C.InputStream
+import Language.C.System.Preprocess
+
+-- | preprocess and parse C file
+--   Synopsis: @parseFile preprocesssor tmp-dir? cpp-opts file@
+parseFile :: (Preprocessor cpp) => cpp -> (Maybe FilePath) -> [String] -> FilePath -> IO (Either ParseError CTranslUnit)
+parseFile cpp tmp_dir_opt args input_file = do
+    let cpp_args = (simpleCppArgs args input_file) { cppTmpDir = tmp_dir_opt }
+    input_stream <- runPreprocessor cpp cpp_args >>= handleCppError
+    return$ parseC input_stream (Position input_file 1 1)
+    where
+    handleCppError (Left exitCode) = fail $ "Preprocessor failed with exit code " ++ show exitCode
+    handleCppError (Right ok)      = return ok

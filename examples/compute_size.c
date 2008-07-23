3585d22e2a3cef4ba4ecc4bb9ff481271f4fe18c
benedikt.huber <benedikt.huber@gmail.com>
2008-07-23 22:05:17 +0000

    Updates to the ComputeSize example

--- examples/compute_size.c
+++ examples/compute_size.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-struct t { char x; short y; } __attribute__((packed));
-typedef struct t T;
+typedef struct t { char x; short y; } __attribute__((packed)) T;
 union u { T x; T* y; };
 struct s { 
   struct k { short b1 : 8, b2: 9, b3: 8, b4 : 7;} x; 
@@ -10,6 +9,6 @@ int main()
 {
   printf("%d\n",sizeof(struct k));
   printf("%d\n",sizeof(struct s));
-  printf("%d\n",sizeof(struct t));
+  printf("%d\n",sizeof(T));
   printf("%d\n",sizeof(union u));
 }

2b8e053bd78be7ac96ea4b8024fe85b34d04b28c
benedikt.huber <benedikt.huber@gmail.com>
2008-07-23 17:14:27 +0000

    Add Language.C.Data.hs

---
+++ src/Language/C/Data.hs
@@ -0,0 +1,35 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Language.C.Data
+-- Copyright   :  (c) 2008 Benedikt Huber
+-- License     :  BSD-style
+-- Maintainer  :  benedikt.huber@gmail.com
+-- Stability   :  experimental
+--
+-- Data structures for Language.C: Identifiers, Source Code Positions,
+-- Extensible Errors and Unique Names.
+-----------------------------------------------------------------------------
+module Language.C.Data (
+     -- * Identifiers
+     SUERef(..),
+     Ident,mkIdent, identToString, internalIdent, isInternalIdent, builtinIdent,
+     -- * Unqiue names
+     Name(..),namesStartingFrom,
+     -- * Source code positions
+     Position(..),Pos(..),
+     posFile,posRow,posColumn,
+     nopos,builtinPos,internalPos,
+     isSourcePos,isBuiltinPos,isInternalPos,
+     -- * Syntax tree nodes
+     NodeInfo(..),CNode(..),
+     fileOfNode,posOfNode,nameOfNode,
+     mkUndefNodeInfo,mkNodeInfoOnlyPos,mkNodeInfo,
+     -- * extensible errors
+     module Language.C.Data.Error
+)
+where
+import Language.C.Data.Ident
+import Language.C.Data.Name
+import Language.C.Data.Position
+import Language.C.Data.Error
+import Language.C.Data.Node

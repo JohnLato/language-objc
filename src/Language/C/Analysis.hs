afd27da91056a0d7f2d667d32c334cc440ed8901
benedikt.huber <benedikt.huber@gmail.com>
2008-08-11 11:13:49 +0000

    Doc Cleanup: Analysis.hs

--- src/Language/C/Analysis.hs
+++ src/Language/C/Analysis.hs
@@ -5,21 +5,28 @@
 -- Copyright   :  (c) 2008 Benedikt Huber
 -- License     :  BSD-style
 -- Maintainer  :  benedikt.huber@gmail.com
--- Stability   :  alpha, unstable
+-- Stability   :  alpha
 --
--- Analysis of the AST.
+-- Analysis of the AST. 
+--
+-- Currently, we provide a monad for analysis and analyze declarations and types.
+-- Especially note that there is no direct support for analyzing function bodies and
+-- constant expressions.
 --
 -- /NOTE/ This is an experimental interface, which will certainly change.
 -----------------------------------------------------------------------------
 module Language.C.Analysis (
+    -- * Semantic representation
     module Language.C.Analysis.SemRep,
+    -- * Error datatypes for the analysis
     module Language.C.Analysis.SemError,
-
+    -- * Traversal monad
     module Language.C.Analysis.TravMonad,
-
+    -- * Top level analysis
     module Language.C.Analysis.AstAnalysis,
+    -- * Analyzing declarations
     module Language.C.Analysis.DeclAnalysis,
-
+    -- * Debug print
     module Language.C.Analysis.Debug,
 )
 where

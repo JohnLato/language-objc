cfbd460f6a45fc4db1eb01e7cd095afd00c4d19f
benedikt.huber <benedikt.huber@gmail.com>
2008-08-11 13:42:56 +0000

    examples: remove QuickTest (integrated into ScanFile). Update ScanFile

--- examples/ScanFile.hs
+++ examples/ScanFile.hs
@@ -1,40 +1,72 @@
 -- Simple example demonstrating the API: parse a file, and print its definition table
 module Main where
-import System.Environment ; import System.FilePath
+import System.Environment
+import System.FilePath
+import System.Exit
 import System.IO
-import Control.Arrow      ; import Control.Monad
+import Control.Arrow      hiding ((<+>))
+import Control.Monad
 import Debug.Trace
+import Text.PrettyPrint.HughesPJ
 
 import Language.C              -- simple API
 import Language.C.Analysis     -- analysis API
 import Language.C.System.GCC   -- preprocessor used
 
+usageMsg :: String -> String
+usageMsg prg = render $
+  text "Usage:" <+> text prg <+> hsep (map text ["CPP_OPTIONS","input_file.c"]) $+$ 
+  (nest 4 $
+    text "Environment Variables" $+$
+      (nest 4 $ 
+         hsep [text "TRACE_EVENTS", text "trace definition events as they occur"]
+      )
+  )
+
 main :: IO ()
 main = do
-    let usage = error "Example Usage: ./ScanFile -I/usr/include my_file.c"
+    let usageErr = (hPutStrLn stderr (usageMsg "./ScanFile") >> exitWith (ExitFailure 1))
     args <- getArgs
-    when (length args < 1) usage
-
+    when (length args < 1) usageErr
+    doTraceDecls <- liftM (("TRACE_EVENTS" `elem`). map fst) getEnvironment
     -- get cpp options and input file
     let (opts,c_file) = (init &&& last) args
 
     -- parse
-    ast                <- parseFile (newGCC "gcc") Nothing opts c_file
-                          >>= checkResult "[parsing]"
+    ast <- errorOnLeftM "Parse Error" $
+      parseFile (newGCC "gcc") Nothing opts c_file
+
     -- analyze
-    print (pretty ast)
-    
-    (globals,warnings) <- (runTrav_ >>> checkResult "[analysis]") $ analyseAST ast
+    (globals,warnings) <- errorOnLeft "Semantic Error" $ runTrav_ $ traversal doTraceDecls ast
 
     -- print
     mapM (hPutStrLn stderr . show) warnings
-    print $ pretty globals -- (filterGlobalDecls (fileOfInterest c_file . fileOfNode) globals)    
+    print $ pretty $ filterGlobalDecls (fileOfInterest c_file . fileOfNode) globals
 
     where
+    traversal False ast = analyseAST ast
+    traversal True  ast = withExtDeclHandler (analyseAST ast) $ \ext_decl ->
+                          trace (declTrace ext_decl) (return ())
+
     checkResult :: (Show a) => String -> (Either a b) -> IO b
     checkResult label = either (error . (label++) . show) return
     fileOfInterest c_file file_name = fileOfInterest' (splitExtension c_file) (splitExtension file_name)
-    fileOfInterest' (c_base,c_ext) (f_base,f_ext) | c_base /= f_base = trace (show $ (c_base,c_ext,f_base,f_ext)) False
-                                                  | f_ext == ".i"    = trace (show $ (c_base,c_ext,f_base,f_ext)) True
-                                                  | c_ext == f_ext   = trace (show $ (c_base,c_ext,f_base,f_ext)) True
+    fileOfInterest' (c_base,c_ext) (f_base,f_ext) | c_base /= f_base = False
+                                                  | f_ext == ".i"    = True
+                                                  | c_ext == f_ext   = True
                                                   | otherwise        = error ("?")
+
+errorOnLeft :: (Show a) => String -> (Either a b) -> IO b
+errorOnLeft msg = either (error . ((msg ++ ": ")++).show) return
+errorOnLeftM msg action = action >>= errorOnLeft msg
+
+declTrace :: DeclEvent -> String
+declTrace event = render $ case event of
+                                TagEvent tag_def     -> (text ("Tag: " ++ show (sueRef tag_def)) <+> file tag_def)
+                                DeclEvent ident_decl -> (text ("Decl: " ++ show (declIdent ident_decl)) <+> file ident_decl)
+                                TypeDefEvent tydef   -> (text ("Typedef: " ++ show (identOfTypeDef tydef)) <+> file tydef)
+                                AsmEvent block       -> (text $ "Assembler block")
+    where
+    file :: (CNode a) => a -> Doc
+    file = text . show . posOfNode . nodeInfo
+                                                  

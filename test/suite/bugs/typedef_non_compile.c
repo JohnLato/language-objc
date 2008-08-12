10d86857cbf473da23017c9fb4a6139d87db718d
benedikt.huber <benedikt.huber@gmail.com>
2008-08-12 16:02:18 +0000

    remove typedef_non_compile (ok if the parser accepts it)

--- test/suite/bugs/typedef_non_compile.c
+++
@@ -1,4 +0,0 @@
-typedef int x; // x is now a typedef-ident
-int y;         // y is now an ident
-int x;         /* Should be hard error: redeclared as different kind of symbol  */
-typedef int y; /* Should be hard error: redeclared as different kind of symbol  */

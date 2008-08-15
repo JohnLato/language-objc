a0c0387a65b03697ee6bfeeb300d1bdfc65726b1
benedikt.huber <benedikt.huber@gmail.com>
2008-08-15 14:37:08 +0000

    CTest: use the exposed parsers expressionP, statementP etc. for parsing single expressions, statements etc.

--- test/src/CTest.hs
+++ test/src/CTest.hs
@@ -11,8 +11,8 @@
 --
 -- It provides a wrapper for parsing C-files which haven't been preprocessed yet.
 -- It is used as if gcc was called, and internally calls cpp (gcc -E) to preprocess the file.
--- It then outputs the pretty printed AST, replacing declarations from included header 
--- files with a corresponding #include directive (This isn't always correct, as e.g. #define s 
+-- It then outputs the pretty printed AST, replacing declarations from included header
+-- files with a corresponding #include directive (This isn't always correct, as e.g. #define s
 -- get lost. But it makes it a lot easier to focus on the relevant part of the output).
 --
 -- If used with a `-e str' command-line argument, the given string is parsed as an expression and pretty
@@ -22,6 +22,7 @@ module Main (
 main
 )  where
 import Language.C
+import Language.C.Parser
 import Language.C.System.GCC
 import Language.C.Analysis
 import Language.C.Test.Environment
@@ -34,8 +35,39 @@ import System.IO
 import Data.Generics
 import Text.PrettyPrint.HughesPJ
 
-data CTestConfig = CTestConfig { debugFlag :: Bool, parseOnlyFlag :: Bool, useIncludes :: Bool,
-                                 dumpAst :: Bool, semanticAnalysis :: Bool }
+data CTestConfig =
+  CTestConfig {
+    debugFlag :: Bool,
+    parseOnlyFlag :: Bool,
+    useIncludes :: Bool,
+    dumpAst :: Bool,
+    semanticAnalysis :: Bool
+  }
+
+usage :: String -> IO a
+usage msg = printUsage >> exitWith (ExitFailure 2) where
+  printUsage = hPutStr stderr . unlines $
+      [ "! "++msg,"",
+        "Usage: ./CTest -e expression",
+        "Usage: ./CTest -s statement",
+        "Usage: ./CTest -d declaration",
+        "Usage: ./CTest [cpp-opts] file.(c|hc|i)",
+        "   parses the given C source file and pretty print the AST",
+        "Environment Variables (some do not apply with -e,-s or -d): ",
+        "   TMPDIR: temporary directory for preprocessing",
+        "   NO_HEADERS_VIA_INCLUDE: do not use heuristic #include directives for pretty printing",
+        "   DEBUG:  debug flag",
+        "   DUMP_AST:  dump the ast to file dump.ast",
+        "   NO_SEMANTIC_ANALYSIS: do not perform semantic analysis",
+        "   PARSE_ONLY: do not pretty print"
+      ]
+
+bailOut :: (Show err) => err -> IO a
+bailOut err = do
+    hPutStrLn stderr (show err)
+    hPutStrLn stderr "*** Exit on Error ***"
+    exitWith (ExitFailure 1)
+
 main :: IO ()
 main = do
   tmpdir     <- getEnv "TMPDIR"
@@ -46,44 +78,32 @@ main = do
   semantic  <- liftM not (getEnvFlag "NO_SEMANTIC_ANALYSIS")
   let config = CTestConfig dbg parseonly (not $ no_includes) dumpast semantic
   args <- getArgs
-  let stdinPos = Position "<stdin>" 1 1
   (file,ast) <-
     case args of
-      ("-e":str:[]) -> either bailOut (return.((,) ""))
-                         (parseC (inputStreamFromString (exprInput str)) stdinPos)
-      ("-d":str:[]) -> either bailOut (return.((,) ""))
-                         (parseC (inputStreamFromString (exprInput str)) stdinPos)
+      ("-e":str:[]) -> runP config expressionP str >> exitWith ExitSuccess
+      ("-s":str:[]) -> runP config statementP str >> exitWith ExitSuccess
+      ("-d":str:[]) -> runP config extDeclP str >> exitWith ExitSuccess
       otherArgs ->
           case mungeCcArgs args of
             Groked [cFile] gccOpts -> do
-                parseCFile (newGCC "gcc") (Just tmpdir) gccOpts cFile >>= either bailOut (return.((,) cFile))
+                presult <- parseCFile (newGCC "gcc") (Just tmpdir) gccOpts cFile
+                either bailOut (return.((,) cFile)) presult
             Groked cFiles _ -> usage $ "More than one source file given: " ++ unwords cFiles
             Ignore -> usage $ "Not input files given"
             Unknown reason -> usage $ "Could not process arguments: " ++ reason
   output config file ast
-bailOut :: (Show err) => err -> IO a
-bailOut err = do
-    hPutStrLn stderr (show err)
-    hPutStrLn stderr "*** Exit on Error ***"
-    exitWith (ExitFailure 1)
-usage :: String -> IO a
-usage msg = printUsage >> exitWith (ExitFailure 2) where
-  printUsage = hPutStr stderr . unlines $ 
-      [ "! "++msg,"",
-        "Usage: ./CTest -e expression",
-        "Usage: ./CTest -d declaration",
-        "Usage: ./CTest [cpp-opts] file.(c|hc|i)",
-        "   parses the given C source file and pretty print the AST",
-        "Environment Variables: ",
-        "   TMPDIR: temporary directory for preprocessing",
-        "   NO_HEADERS_VIA_INCLUDE: do not use heuristic #include directives for pretty printing",
-        "   DEBUG:  debug flag",
-        "   DUMP_AST:  dump the ast to file dump.ast",
-        "   NO_SEMANTIC_ANALYSIS: do not perform semantic analysis",
-        "   PARSE_ONLY: do not pretty print"
-      ]
-exprInput str = "void *x = " ++ str ++ " ;"
-declInput str = str ++ ";"
+
+runP :: (CNode a, Pretty a, Data a) => CTestConfig -> P a -> String -> IO ()
+runP config parser str =
+  do
+    ast <- either bailOut return $ pResult
+    when (dumpAst config) $ writeFile "dump.ast" (gshow ast)
+    when (not $ parseOnlyFlag config) $ print (pretty ast)
+  where
+  is = inputStreamFromString str
+  pResult = execParser_ parser is (argPos)
+  argPos = Position "<cmd-line-arg>" 1 1
+
 output :: CTestConfig -> FilePath -> CTranslUnit -> IO ()
 output config file ast = do
     when (dumpAst config) $ writeFile "dump.ast" (gshow ast)
@@ -93,8 +113,11 @@ output config file ast = do
             Left errs -> hPutStrLn stderr (show errs)
             Right (ok,warnings) -> do mapM (hPutStrLn stderr . show) warnings
                                       printStats file ok
-    when (not $ parseOnlyFlag config) $ print $ (if useIncludes config then prettyUsingInclude else pretty) ast
+    when (not $ parseOnlyFlag config) $
+      print $ (if useIncludes config then prettyUsingInclude else pretty) ast
     when (debugFlag config) $ putStrLn . comment . show . pretty . mkGenericCAST $ ast
-comment str = "/*\n" ++ str ++ "\n*/"
-printStats file = putStrLn . comment . show . prettyAssocsWith "global decl stats" text (text.show) . globalDeclStats (== file)
 
+comment str = "/*\n" ++ str ++ "\n*/"
+printStats file = putStrLn . comment . show
+                  . prettyAssocsWith "global decl stats" text (text.show)
+                  . globalDeclStats (== file)

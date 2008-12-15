c637dc14f803a650870a823fd2cafa8152aff6f4
Aaron Tomb <aarontomb@gmail.com>
2008-12-15 23:41:46 +0000

    Move builtins to a separate module.

--- src/Language/C/Analysis/TravMonad.hs
+++ src/Language/C/Analysis/TravMonad.hs
@@ -46,6 +46,7 @@ import Language.C.Data
 import Language.C.Data.RList as RList
 import Language.C.Syntax
 
+import Language.C.Analysis.Builtins
 import Language.C.Analysis.SemError
 import Language.C.Analysis.SemRep
 import Language.C.Analysis.DefTable hiding (enterBlockScope,leaveBlockScope,
@@ -351,130 +352,6 @@ get      = Trav (\s -> Right (s,s))
 put :: TravState s -> Trav s ()
 put s    = Trav (\_ -> Right ((),s))
 
-builtins :: DefTable
-builtins = foldr doIdent (foldr doTypeDef emptyDefTable typedefs) idents
-  where doTypeDef d = snd . defineTypeDef (identOfTypeDef d) d
-        doIdent   d = snd . defineGlobalIdent (declIdent d) d
-        dName     s = VarName (internalIdent s) Nothing
-        param ty    = ParamDecl (VarDecl
-                                 NoName
-                                 (DeclAttrs False (Auto False) [])
-                                 ty) undefNode
-        fnAttrs     = DeclAttrs False (FunLinkage ExternalLinkage) []
-        varAttrs    = DeclAttrs False (Static InternalLinkage False) []
-        fnType r as = FunctionType (FunType r (map param as) False [])
-        func n r as = Declaration
-                      (Decl
-                       (VarDecl (dName n) fnAttrs (fnType r as))
-                       undefNode)
-        var n t     = Declaration
-                      (Decl (VarDecl (dName n) varAttrs t) undefNode)
-        typedef n t = TypeDef (internalIdent n) t [] undefNode
-        integral ty = DirectType (TyIntegral ty) noTypeQuals
-        floating ty = DirectType (TyFloating ty) noTypeQuals
-        stringType  = ArrayType
-                      (DirectType (TyIntegral TyChar)
-                                    (TypeQuals True False False))
-                      (UnknownArraySize False)
-                      noTypeQuals
-                      []
-        voidPtr     = PtrType (DirectType TyVoid noTypeQuals) noTypeQuals []
-        constVoidPtr = PtrType
-                       (DirectType TyVoid noTypeQuals)
-                       (TypeQuals True False False)
-                       []
-        constCharPtr = PtrType
-                       (integral TyChar)
-                       (TypeQuals True False False)
-                       []
-        charPtr     = PtrType (integral TyChar) noTypeQuals []
-        voidType    = DirectType TyVoid noTypeQuals
-        valistType  = DirectType (TyBuiltin TyVaList) noTypeQuals
-        sizeType    = integral TyInt -- XXX: really size_t
-        typedefs    = [ typedef "__builtin_va_list"
-                                valistType
-                      ]
-        idents      = [ func "__builtin_expect"
-                             (integral TyLong)
-                             [integral TyLong, integral TyLong]
-                      , func "__builtin_fabs"
-                             (floating TyDouble)
-                             [floating TyDouble]
-                      , func "__builtin_fabsf"
-                             (floating TyFloat)
-                             [floating TyFloat]
-                      , func "__builtin_fabsl"
-                             (floating TyLDouble)
-                             [floating TyLDouble]
-                      , func "__builtin_inf" (floating TyDouble) []
-                      , func "__builtin_inff" (floating TyFloat) []
-                      , func "__builtin_infl" (floating TyLDouble) []
-                      , func "__builtin_huge_val" (floating TyDouble) []
-                      , func "__builtin_huge_valf" (floating TyFloat) []
-                      , func "__builtin_huge_vall" (floating TyLDouble) []
-                      , func "__builtin_va_start"
-                             voidType
-                             [ valistType , voidPtr ]
-                      , func "__builtin_va_end"
-                             voidType
-                             [valistType]
-                      , func "__builtin_va_copy"
-                             voidType
-                             [ valistType, valistType ]
-                      , func "__builtin_alloca"
-                             voidPtr
-                             [ sizeType ]
-                      , func "__builtin_memcpy"
-                             voidPtr
-                             [ voidPtr
-                             , constVoidPtr
-                             , sizeType
-                             ]
-                      , func "__builtin_strchr"
-                             charPtr
-                             [ constCharPtr, integral TyInt]
-                      , func "__builtin_strncpy"
-                             charPtr
-                             [ constCharPtr -- XXX: restrict
-                             , constCharPtr -- XXX: restrict
-                             , sizeType
-                             ]
-                      , func "__builtin_strncat"
-                             charPtr
-                             [ constCharPtr -- XXX: restrict
-                             , constCharPtr -- XXX: restrict
-                             , sizeType
-                             ]
-                      , func "__builtin_strcmp"
-                             (integral TyInt)
-                             [ constCharPtr, constCharPtr ]
-                      , func "__builtin_bzero"
-                             voidType
-                             [ voidPtr, sizeType ]
-                      , func "__builtin_constant_p"
-                             (integral TyInt)
-                             [DirectType (TyBuiltin TyAny) noTypeQuals]
-                      -- XXX: I don't know if the following has the
-                      -- correct type. It doesn't seem to be
-                      -- documented.
-                      , func "__builtin_extract_return_addr"
-                             voidPtr
-                             [ voidPtr ]
-                      , func "__builtin_return_address"
-                             voidPtr
-                             [ integral TyUInt ]
-                      , func "__builtin_frame_address"
-                             voidPtr
-                             [ integral TyUInt ]
-                      , func "__builtin_expect"
-                             (integral TyLong)
-                             [ integral TyLong, integral TyLong ]
-                      , func "__builtin_prefetch"
-                             voidType
-                             [ constVoidPtr ]
-                      , var "__func__"
-                            stringType
-                      ]
 
 runTrav :: forall s a. s -> Trav s a -> Either [CError] (a, TravState s)
 runTrav state traversal =

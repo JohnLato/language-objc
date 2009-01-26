5ed22f7643a1d73299e32cd539cb23115844e611
joe.thornber <joe.thornber@gmail.com>
2009-01-26 14:35:42 +0000

    [Position] Use record syntax

--- src/Language/C/Data/Position.hs
+++ src/Language/C/Data/Position.hs
@@ -26,45 +26,26 @@ module Language.C.Data.Position (
 ) where
 import Data.Generics
 
--- | uniform representation of source file positions 
-data Position = Position 
-        {-# UNPACK #-}   !Int           -- offset/id
-        String                          -- file name
-        {-# UNPACK #-}   !Int           -- row
-        {-# UNPACK #-}   !Int           -- column (remove ?)
-        | NoPosition
-        | BuiltinPosition
-        | InternalPosition
-  deriving (Eq, Ord, Typeable, Data)
+-- | uniform representation of source file positions
+data Position = Position { posOffset :: {-# UNPACK #-} !Int
+                         , posFile :: String
+                         , posRow :: {-# UNPACK #-} !Int
+                         , posColumn :: {-# UNPACK #-} !Int
+                         }
+              | NoPosition
+              | BuiltinPosition
+              | InternalPosition
+                deriving (Eq, Ord, Typeable, Data)
 
 type PosLength = (Position,Int)
 
 instance Show Position where
-  show pos@(Position _ fname row _)
-    | isNoPos pos = "<no file>"
-    | isBuiltinPos pos = "<builtin>"
-    | isInternalPos pos = "<internal>"
-    | otherwise = "(" ++ show fname ++ ": line " ++ show row ++ ")"
-    
--- | get the source file of the specified position. Fails unless @isSourcePos pos@.
-posFile :: Position -> String
-posFile (Position _ fname _ col) = fname
-
--- | get the line number of the specified position. Fails unless @isSourcePos pos@
-posRow  :: Position -> Int
-posRow (Position _ _ row col) = row
+  show (Position _ fname row _) = "(" ++ show fname ++ ": line " ++ show row ++ ")"
+  show NoPosition               = "<no file>"
+  show BuiltinPosition          = "<builtin>"
+  show InternalPosition         = "<internal>"
 
 {-# DEPRECATED posColumn "column number information is inaccurate in presence of macros - do not rely on it." #-}
-
--- | get the column of the specified position.
--- Has been removed, as column information is inaccurate before preprocessing,
--- and meaningless afterwards (because of #LINE pragmas).
-posColumn :: Position -> Int
-posColumn (Position _ _ _ col) = col
-
--- | get the absolute offset of the position (in the preprocessed source)
-posOffset :: Position -> Int
-posOffset (Position off _ _ col) = off
 
 -- | class of type which aggregate a source code location
 class Pos a where
@@ -77,11 +58,11 @@ initPos file = Position 0 file 1 1
 -- | returns @True@ if the given position refers to an actual source file
 isSourcePos :: Position -> Bool
 isSourcePos (Position _ _ _ _) = True
-isSourcePos _                = False
+isSourcePos _                  = False
 
 -- | no position (for unknown position information)
 nopos :: Position
-nopos  = Position (-1) "<no file>" (-1) 0 
+nopos  = NoPosition
 
 -- | returns @True@ if the there is no position information available
 isNoPos :: Position -> Bool
@@ -111,9 +92,18 @@ isInternalPos _                = False
 -- | advance column
 incPos :: Position -> Int -> Position
 incPos (Position offs fname row col) n = Position (offs + n) fname row (col + n)
-incPos p _                              = p
+incPos p _                             = p
 
 {-# INLINE retPos #-}
 -- | advance to next line
 retPos :: Position -> Position
 retPos (Position offs fname row _) = Position (offs+1) fname (row + 1) 1
+retPos p                           = p
+
+{-# INLINE adjustPos #-}
+-- | adjust position: change file and line number, reseting column to 1. This is usually
+--   used for #LINE pragmas. The absolute offset is not changed - this can be done
+--   by @adjustPos newFile line . incPos (length pragma)@.
+adjustPos :: FilePath -> Int -> Position -> Position
+adjustPos fname row (Position offs _ _ _) = Position offs fname row 1
+adjustPos _ _ p                           = p

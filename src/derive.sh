86a67c5d3c514dd5712c9548a03afe7d28efd397
benedikt.huber <benedikt.huber@gmail.com>
2011-05-02 14:15:13 +0000

    Automated Derivations: Use derive-2.5 if available (no need to patch derive-2.4)

--- src/derive.sh
+++ src/derive.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -o errexit
-DERIVE=../scripts/derive
-DERIVE_VERSION=2.4.2
+DERIVE=./derive/Derive
+DERIVE_PATCH_VERSION=2.4.2
+if ghc-pkg find-module Data.DeriveMain | grep -q '^[ ]*derive-'; then
+    (cd derive && ghc -O --make -o Derive Derive.hs)
+fi
 if [ ! -e ${DERIVE} ] ; then
-	echo "Warning: Could not find ${DERIVE}. Press Enter to download, patch, build and install it."
+	echo "Warning: Could not find ${DERIVE}, and derive >= 2.5 is not installed">&2
+        echo "Press Enter to download, patch and build derive-${DERIVE_PATCH_VERSION}.">&2
 	read
-	cabal unpack derive-${DERIVE_VERSION}
-	pushd derive-${DERIVE_VERSION}
-	patch -p1 < ../derive-${DERIVE_VERSION}.patch
+	cabal unpack derive-${DERIVE_PATCH_VERSION}
+	pushd derive-${DERIVE_PATCH_VERSION}
+	patch -p1 < ../derive-${DERIVE_PATCH_VERSION}.patch
 	cabal configure
 	cabal build
 	popd
 	echo "Installing ${DERIVE}"
-	cp derive-${DERIVE_VERSION}/dist/build/derive/derive "${DERIVE}"
+	cp derive-${DERIVE_PATCH_VERSION}/dist/build/derive/derive "${DERIVE}"
 fi
 TARGETS="Language/C/Syntax/AST.hs Language/C/Analysis/SemRep.hs"
 for T in ${TARGETS} ; do

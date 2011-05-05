24f69f22df3bbe02b9fa7876e540e69aee65af75
bernauer <bernauer@inf.ethz.ch>
2011-05-05 14:51:45 +0000

    pretty_dump_ast
    Using the Haskell parser to pretty print the AST dump of a C sample file

--- examples/DumpAst.hs
+++ examples/DumpAst.hs
@@ -1,40 +1,39 @@
--- Minimal example: parse a file, and print its AST
-module Main where
-import System.Environment
-import System.FilePath
-import System.Exit
-import System.IO
-import Control.Arrow      hiding ((<+>))
-import Control.Monad
-import Debug.Trace
-import Text.PrettyPrint.HughesPJ
-import Data.List
+import Language.Haskell.Pretty (prettyPrint)
+import Language.Haskell.Parser (ParseResult(ParseOk, ParseFailed), parseModule)
 
-import Language.C              -- simple API
-import Language.C.System.GCC   -- preprocessor used
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode(ExitFailure))
+import System.IO (hPutStrLn, stderr)
+import Control.Monad (when)
+import Text.PrettyPrint.HughesPJ (render, text, (<+>), hsep)
+
+import Language.C (parseCFile)
+import Language.C.System.GCC (newGCC)
 
 usageMsg :: String -> String
-usageMsg prg = render $
-  text "Usage:" <+> text prg <+> hsep (map text ["CPP_OPTIONS","input_file.c"])
+usageMsg prg = render $ text "Usage:" <+> text prg <+> hsep (map text ["CPP_OPTIONS","input_file.c"])
 
 main :: IO ()
 main = do
-    let usageErr = (hPutStrLn stderr (usageMsg "./ParseAndPrint") >> exitWith (ExitFailure 1))
-    args <- getArgs
-    when (length args < 1) usageErr
-    let (opts,input_file) = (init args, last args)
-
-    -- parse
-    ast <- errorOnLeftM "Parse Error" $
-      parseCFile (newGCC "gcc") Nothing opts input_file
-    -- dump
-    putStrLn $ show (fmap (const ShowPlaceholder) ast)
+	let usageErr = (hPutStrLn stderr (usageMsg "./ParseAndPrint") >> exitWith (ExitFailure 1))
+	args <- getArgs
+	when (length args < 1) usageErr
+	let (opts,input_file) = (init args, last args)
+	ast <- errorOnLeftM "Parse Error" $ parseCFile (newGCC "gcc") Nothing opts input_file
+	putStrLn $ pprint $ parseModule $ decorate $ show $ fmap (const ShowPlaceholder) ast
 
 errorOnLeft :: (Show a) => String -> (Either a b) -> IO b
 errorOnLeft msg = either (error . ((msg ++ ": ")++).show) return
+
 errorOnLeftM :: (Show a) => String -> IO (Either a b) -> IO b
 errorOnLeftM msg action = action >>= errorOnLeft msg
 
 data ShowPlaceholder = ShowPlaceholder
 instance Show ShowPlaceholder where
   showsPrec _ ShowPlaceholder = showString "_"
+
+pprint (ParseOk hsm) = prettyPrint hsm
+pprint (ParseFailed loc msg) = error $ show loc ++ ": " ++ msg
+
+decorate ast = "foo (" ++ ast ++ ") = undefined"
+

f084f908bfb5c17e17e7559e79b0b5f7814e3428
benedikt.huber <benedikt.huber@gmail.com>
2011-08-16 15:48:28 +0000

    test/suite/bugs: rewrite to make things compile with gcc-4.5

--- test/suite/bugs/assignment_prec_1.c
+++ test/suite/bugs/assignment_prec_1.c
@@ -4,6 +4,7 @@ void foo() {
   int u  = ( y = 2 ? 3 : 0 );  
   int u_ = ( (y = 2) ? 3 : 0 );
   int v = ( y = (2 ? 3 : 0));
-  int w = ( (2 ? x : y) = x);   // Warning (not really an lvalue)
+  /* no longer supported by gcc 4.5 */
+  /* int w = ( (2 ? x : y) = x);   // Warning (not really an lvalue) */
   int s = ( 2 ? 3 : (y = x));
 }
